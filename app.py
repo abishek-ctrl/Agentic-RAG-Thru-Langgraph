8d933e09381bf8fb76334fc7e074e353277ec898
Abishek M <amabishek02@gmail.com>
2023-08-02 22:41:01 +0530

    get text function

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from dotenv import load_dotenv
+from pypdf import PdfReader
+
+
+def get_text(docs):
+    text=""
+    for pdf in docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="PDF Chat", page_icon=":books:")
 
     st.header("PDF Chat :books:")
@@ -8,11 +20,11 @@ def main():
 
     with st.sidebar:
         st.subheader("Your PDFs")
-        st.file_uploader("Upload your PDFs here")
-        st.button("Upload")
-
-
-    
+        docs=st.file_uploader("Upload your PDFs here")
+        if st.button("Upload"):
+            with st.spinner("Processing"):
+                rawtxt=get_text(docs)
+                st.write(rawtxt)
 
 if __name__ == "__main__":
     main()
