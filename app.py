b38b46dcf42cc818fe35dd925bba29858cbdffc4
Abishek <amabishek02@email.com>
2024-11-21 13:27:56 +0530

    changed llm and embeds to nvidia and together

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-import streamlit as st
 import os
+import streamlit as st
+import getpass
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain_mistralai.chat_models import ChatMistralAI
+from langchain_together import Together
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+
 from htmltemp import css, botTemp, userTemp
 
+load_dotenv()
+
 def get_text(docs):
-    text= ""
+    text = ""
     for pdf in docs:
-        pdf_reader=PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+=page.extract_text()
+            text += page.extract_text()
     return text
 
 def get_chunks(rawtxt):
-    splitter=CharacterTextSplitter(
+    splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks=splitter.split_text(rawtxt)
+    chunks = splitter.split_text(rawtxt)
     return chunks
 
 def get_vectorstore(chunks):
-    openai_api_key = os.environ.get('OPENAI_API_KEY')
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key) 
-    vectors=FAISS.from_texts(texts=chunks,embedding=embeddings)
+    embeddings = NVIDIAEmbeddings(model="NV-Embed-QA")
+    vectors = FAISS.from_texts(chunks, embedding=embeddings)
     return vectors
 
 def get_convo_chain(vector):
-    llm = ChatMistralAI()  # Use ChatMistralAI instead of ChatOpenAI
+    llm = Together(
+        model="meta-llama/Meta-Llama-3-8B-Instruct-Turbo",
+        temperature=0.7,
+        max_tokens=100
+    )
+
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     convo_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -45,41 +53,43 @@ def get_convo_chain(vector):
     return convo_chain
 
 def handle_inp(inp):
-    response=st.session_state.conversation({'question':inp})
-    st.session_state.chat_history= response['chat_history']
-
+    response = st.session_state.conversation({'question': inp})
+    st.session_state.chat_history = response['chat_history']
     for i, msg in enumerate(st.session_state.chat_history):
-        if i%2==0:
-            st.write(userTemp.replace("{{MSG}}",msg.content),unsafe_allow_html=True)
+        if i % 2 == 0:
+            st.write(userTemp.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
         else:
-            st.write(botTemp.replace("{{MSG}}",msg.content),unsafe_allow_html=True)
-
+            st.write(botTemp.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+    st.session_state.input = ""
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="PDF Chat", page_icon=":books:")
 
-    st.write(css,unsafe_allow_html=True)
+    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
-        st.session_state.conversation=None
+        st.session_state.conversation = None
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history=None
+        st.session_state.chat_history = None
+    if "input" not in st.session_state:
+        st.session_state.input = ""
 
     st.header("PDF Chat :books:")
-    inp= st.text_input("Ask a question about the PDF:")
+    inp = st.text_input("Ask a question about the PDF:", value=st.session_state.input)
     if inp:
         handle_inp(inp)
 
     with st.sidebar:
         st.subheader("Your PDFs")
-        docs=st.file_uploader("Upload your PDFs here",accept_multiple_files=True)
+        docs = st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
+
         if st.button("Upload"):
             with st.spinner("Processing"):
-                rawtxt=get_text(docs)
-                chunks=get_chunks(rawtxt)
-                vectors=get_vectorstore(chunks)
-                st.session_state.conversation=get_convo_chain(vectors)
+                rawtxt = get_text(docs)
+                chunks = get_chunks(rawtxt)
+                vectors = get_vectorstore(chunks)
+                st.session_state.conversation = get_convo_chain(vectors)
 
 if __name__ == "__main__":
     main()
